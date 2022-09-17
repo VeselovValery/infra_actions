db5176d434f10eaa380bc3b7ac4a199fbd3f6683
Manik <veselov.manik86@yandex.ru>
2022-09-17 12:49:02 +0300

    new commit

---
+++ infra_project/infra_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
